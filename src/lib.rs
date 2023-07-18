5d22bd9d72755602366a3623c4adef22dae46b34
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2023-07-18 10:52:47 +0100

    worldcoord

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use arrayvec::ArrayVec;
+
+const MAX_DIMS: usize = 5;
+pub type Coord<T> = ArrayVec<T, MAX_DIMS>;
+pub type RealCoord = Coord<f64>;
+
 #[cfg(any(feature = "v0_4", feature = "v0_5"))]
 mod util;
 
